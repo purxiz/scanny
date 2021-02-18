8c9054f3c6474f971b9c7e1528b9e2821d3315c4
purxiz <functionofn@gmail.com>
2021-02-18 10:06:58 -0600

    search bugfix
    Fixed an issue where after searching on the main page, the full list wouldn't always re-appear after deleting a search string.

--- templates/index.js
+++ templates/index.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 	$('#search').keyup(function() {
 		let term = $('#search').val();
-		if(term.length < 3) return;
+		if(term.length < 3) {
+			$('#left').children().show();
+			return;
+		}
 		let items = $('#left').children().hide().each(function() {
 			if($(this).text().toLowerCase().indexOf(term) > -1) $(this).show();
 		});
